ffb2373dc40c7430f5a8a9a1af213a94743ac3f4
liamallen343 <liamallen343@gmail.com>
2023-06-03 14:03:01 -0400

    Added phone number slider

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+document.getElementById("enjoy-btn").addEventListener("click", function(){
+    window.location.assign("/features.html")
+})
+
+setInterval(function(){
+    let slideNumber = document.getElementById("slide-number-box").value
+}, 100)
+
+
+document.getElementById("slide-number-box").innerHTML = SlideNumber
